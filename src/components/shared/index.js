99578f278ea0dc81637853b45b5c10e6beace1f6
TIvers <wlad2000vita@gmail.com>
2024-12-21 00:07:36 +0700

    Изменение ветки под back

--- src/components/shared/index.js
+++
@@ -1,5 +0,0 @@
-export { Container } from "./Container";
-export { Header } from "./Header";
-export { Sidebar } from "./Sidebar";
-export { Products } from "./Products";
-export { Product } from "./Product";
--- src/utils/constants.js
+++
@@ -1,6 +0,0 @@
-export const normal = "normal";
-export const outline = "outline";
-export const basket = "basket";
-export const quantity = "quantity";
-export const minus = "minus";
-export const plus = "plus";
