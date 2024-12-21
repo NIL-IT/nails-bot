937115d74a58e3e43dd71363595ed7a6b2d7ad38
TIvers <wlad2000vita@gmail.com>
2024-12-21 15:40:06 +0700

    Добавление запросов

--- src/components/routes/AppRoutes.jsx
+++ src/components/routes/AppRoutes.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import { ROUTES } from "./routes";
@@ -7,9 +7,109 @@ import SingleCategory from "../pages/SingleCategory";
 import Cart from "../pages/Cart";
 import SearchItems from "../pages/SearchItems";
 import OrderHistory from "../pages/OrderHistory";
-import { USERS } from "../../utils/data";
-import OrderHistoryAdmin from "../pages/OrderHistoryAdmin";
-const AppRoutes = () => {
+import OrderHistoryAdmin from "../pages/OrderHistoryAdmin"; // Admin version of OrderHistory
+
+const AppRoutes = ({ user }) => {
+  const [categories, setCategories] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [orders, setOrders] = useState([]);
+
+  const baseURL = "https://testingnil6.ru:8000/";
+
+  const API = {
+    // Получение данных пользователя
+    getUser: async (id_tg) => {
+      const option = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ id_tg }),
+      };
+  
+      return fetch(`${baseURL}get_user.php`, option)
+        .then((res) => res.json())
+        .catch((err) => {
+          console.error("API request error:", err);
+          return { success: false };
+        });
+    },
+  
+    // Получение категорий
+    getCategories: async () => {
+      const option = {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+  
+      return fetch(`${baseURL}categories.php`, option)
+        .then((res) => res.json())
+        .catch((err) => {
+          console.error("API request error:", err);
+          return [];
+        });
+    },
+  
+    // Получение продуктов
+    getProducts: async () => {
+      const option = {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+  
+      return fetch(`${baseURL}products.php`, option)
+        .then((res) => res.json())
+        .catch((err) => {
+          console.error("API request error:", err);
+          return [];
+        });
+    },
+  
+    // Получение заказов пользователя
+    getOrders: async (userId) => {
+      const option = {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+  
+      return fetch(`${baseURL}orders.php?user_id=${userId}`, option)
+        .then((res) => res.json())
+        .catch((err) => {
+          console.error("API request error:", err);
+          return [];
+        });
+    },
+  };
+
+  const fetchCategories = async () => {
+    const categoriesData = await API.getCategories();
+    setCategories(categoriesData);
+  };
+
+  const fetchProducts = async () => {
+    const productsData = await API.getProducts();
+    setProducts(productsData);
+  };
+
+  const fetchOrders = async () => {
+    if (user?.id) {
+      const ordersData = await API.getOrders(user.id);
+      setOrders(ordersData);
+    }
+  };
+
+  useEffect(() => {
+    fetchCategories();
+    fetchProducts();
+    fetchOrders();
+  }, [user]);
+
   return (
     <Routes>
       <Route path={ROUTES.HOME} element={<Home />} />
@@ -17,14 +117,11 @@ const AppRoutes = () => {
       <Route path={ROUTES.CATEGORY} element={<SingleCategory />} />
       <Route path={ROUTES.CART} element={<Cart />} />
       <Route path={ROUTES.SEARCH} element={<SearchItems />} />
-      {USERS[0].isAdmin ? (
-        <>
-          <Route path={ROUTES.PROFILE} element={<OrderHistoryAdmin />} />
-        </>
+
+      {user?.isAdmin ? (
+        <Route path={ROUTES.PROFILE} element={<OrderHistoryAdmin orders={orders} />} />
       ) : (
-        <>
-          <Route path={ROUTES.PROFILE} element={<OrderHistory />} />
-        </>
+        <Route path={ROUTES.PROFILE} element={<OrderHistory orders={orders} />} />
       )}
     </Routes>
   );
