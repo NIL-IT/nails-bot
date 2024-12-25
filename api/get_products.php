c77ad6aa179f9f9b898212e51cb11964b03b3d9d
TIvers <wlad2000vita@gmail.com>
2024-12-25 14:25:11 +0700

    Изменение

--- api/get_products.php
+++ api/get_products.php
@@ -2,14 +2,29 @@
 // Адрес вашего REST вебхука
 $webhookUrl = 'https://shtuchki.pro/rest/1/sjjsknt6sbp1ga92hh8z9ki1naw25ki6';
 
-// Вызов метода для получения списка товаров
-$response = file_get_contents($webhookUrl . '/crm.product.list');
-$products = json_decode($response, true);
+// Задаем параметры для фильтрации (если нужно)
+$params = [
+    'filter' => [], // Фильтры, например ['ACTIVE' => 'Y']
+    'select' => ['ID', 'NAME', 'PRICE', 'QUANTITY'], // Поля, которые хотите получить
+    'start' => 0 // Для постраничной загрузки
+];
 
-if (isset($products['result'])) {
+// Выполняем запрос
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $webhookUrl);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+$response = curl_exec($ch);
+curl_close($ch);
+
+// Обрабатываем ответ
+$data = json_decode($response, true);
+if (isset($data['result'])) {
     echo '<pre>';
-    print_r($products['result']);
+    print_r($data['result']);
     echo '</pre>';
 } else {
-    echo 'Ошибка получения данных';
+    echo 'Ошибка получения данных: ' . $response;
 }
