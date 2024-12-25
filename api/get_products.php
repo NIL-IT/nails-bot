8a804237e3628cf78e17018b4f8f2a25debdf51b
TIvers <wlad2000vita@gmail.com>
2024-12-25 15:44:57 +0700

    2

--- api/get_products.php
+++ api/get_products.php
@@ -2,27 +2,41 @@
 // Указываем правильный URL для вашего API Webhook
 $webhookUrl = "https://shtuchki.pro/rest/13283/hrwfpr2yee7qvk2f/profile/";
 
+// Данные запроса
+$queryData = http_build_query(array(
+    "id" => 1111 // ID товара
+));
+
 // Инициализация cURL
-$ch = curl_init();
+$curl = curl_init();
 
 // Устанавливаем параметры запроса
-curl_setopt($ch, CURLOPT_URL, $webhookUrl);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-    "Content-Type: application/json"
+curl_setopt_array($curl, array(
+    CURLOPT_SSL_VERIFYPEER  => 0,
+    CURLOPT_POST            => 1,
+    CURLOPT_HEADER          => 0,
+    CURLOPT_RETURNTRANSFER  => 1,
+    CURLOPT_URL             => $webhookUrl,
+    CURLOPT_POSTFIELDS      => $queryData,
 ));
 
 // Отправляем запрос
-$response = curl_exec($ch);
+$result = curl_exec($curl);
 
 // Проверяем на ошибки
-if (curl_errno($ch)) {
-    echo 'Ошибка cURL: ' . curl_error($ch);
+if (curl_errno($curl)) {
+    echo 'Ошибка cURL: ' . curl_error($curl);
 } else {
-    // Печатаем результат
-    echo "Ответ от API: " . $response;
+    // Обрабатываем результат
+    $result = json_decode($result, true);
+    if (isset($result['result']['product']['detailPicture']['url'])) {
+        $result['result']['product']['detailPicture']['url'] = str_replace('/rest/', $webhookUrl, $result['result']['product']['detailPicture']['url']);
+    }
+    echo '<pre>';
+    print_r($result);
+    echo '</pre>';
 }
 
 // Закрываем соединение
-curl_close($ch);
+curl_close($curl);
 ?>
