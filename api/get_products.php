724551281a49ac87b6782fac957d9eccf5d4b14d
TIvers <wlad2000vita@gmail.com>
2024-12-25 19:45:00 +0700

    Возврат с ответом

--- api/get_products.php
+++ api/get_products.php
@@ -1,42 +1,29 @@
 <?php
-// Указываем правильный URL для вашего API Webhook
-$webhookUrl = "https://shtuchki.pro/rest/13283/hrwfpr2yee7qvk2f/";
+// Адрес вашего REST вебхука
+$webhookUrl = 'https://shtuchki.pro/rest/13283/hrwfpr2yee7qvk2f/profile/';
 
-// Данные запроса
-$queryData = http_build_query(array(
-    "id" => 1111 // ID товара
-));
+// Задаем параметры для запроса (если нужно)
+$params = [
+    // Укажите необходимые параметры для вашего метода, если они требуются
+];
 
-// Инициализация cURL
-$curl = curl_init();
+// Выполняем запрос
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $webhookUrl);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+$response = curl_exec($ch);
+curl_close($ch);
 
-// Устанавливаем параметры запроса
-curl_setopt_array($curl, array(
-    CURLOPT_SSL_VERIFYPEER  => 0,
-    CURLOPT_POST            => 1,
-    CURLOPT_HEADER          => 0,
-    CURLOPT_RETURNTRANSFER  => 1,
-    CURLOPT_URL             => $webhookUrl,
-    CURLOPT_POSTFIELDS      => $queryData,
-));
-
-// Отправляем запрос
-$result = curl_exec($curl);
-
-// Проверяем на ошибки
-if (curl_errno($curl)) {
-    echo 'Ошибка cURL: ' . curl_error($curl);
-} else {
-    // Обрабатываем результат
-    $result = json_decode($result, true);
-    if (isset($result['result']['product']['detailPicture']['url'])) {
-        $result['result']['product']['detailPicture']['url'] = str_replace('/rest/', $webhookUrl, $result['result']['product']['detailPicture']['url']);
-    }
+// Обрабатываем ответ
+$data = json_decode($response, true);
+if (isset($data['result'])) {
     echo '<pre>';
-    print_r($result);
+    print_r($data['result']);
     echo '</pre>';
+} else {
+    echo 'Ошибка получения данных: ' . $response;
 }
-
-// Закрываем соединение
-curl_close($curl);
 ?>
