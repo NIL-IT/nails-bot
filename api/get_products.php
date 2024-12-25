fcb3138fb660b38146beba163cb2e67c86f99edb
TIvers <wlad2000vita@gmail.com>
2024-12-25 20:13:15 +0700

    Обновлен обработчик cURL в get_products.php: добавлена проверка на ошибки и улучшена обработка ответа.

--- api/get_products.php
+++ api/get_products.php
@@ -1,24 +1,32 @@
 <?php
-// Адрес вашего REST вебхука
+// Адрес REST API
 $webhookUrl = 'https://shtuchki.pro/rest/13283/hrwfpr2yee7qvk2f/profile/';
 
 // Инициализация cURL
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $webhookUrl);
+curl_setopt($ch, CURLOPT_URL, $webhookUrl); // URL запроса
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    'Content-Type: application/json',
+]);
 
 // Выполнение GET-запроса
 $response = curl_exec($ch);
-curl_close($ch);
 
-// Обработка ответа
-$data = json_decode($response, true);
-if (isset($data['result'])) {
-    echo '<pre>';
-    print_r($data['result']);
-    echo '</pre>';
+// Проверка на ошибки
+if (curl_errno($ch)) {
+    echo 'Ошибка cURL: ' . curl_error($ch);
 } else {
-    echo 'Ошибка получения данных: ' . $response;
+    $data = json_decode($response, true);
+    if (isset($data['result'])) {
+        echo '<pre>';
+        print_r($data['result']);
+        echo '</pre>';
+    } else {
+        echo 'Ошибка получения данных: ' . $response;
+    }
 }
+
+// Закрытие cURL
+curl_close($ch);
 ?>
