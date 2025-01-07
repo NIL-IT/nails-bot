b231e93df302b21ab2d6e8d622ca9387b329c528
TIvers <wlad2000vita@gmail.com>
2025-01-07 14:33:39 +0700

    Проверка товаров

--- api/get_products.php
+++ api/get_products.php
@@ -1,30 +1,31 @@
 <?php
-$webhookUrl = 'https://shtuchki.pro/rest/68/zhc69jnwgx6hweyj/profile/';
+// Адрес вашего REST вебхука с указанием метода
+$webhookUrl = 'https://shtuchki.pro/rest/13283/hrwfpr2yee7qvk2f/catalog.product.list';
 
+// Задаем параметры для фильтрации и выбора полей
+$params = [
+    'filter' => [], // Например, фильтр по активности: ['ACTIVE' => 'Y']
+    'select' => ['ID', 'IBLOCK_ID', 'NAME', 'PRICE', 'QUANTITY'], // Обязательные и дополнительные поля
+    'start' => 0 // Для постраничной загрузки, если много данных
+];
+
+// Выполняем запрос
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $webhookUrl);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, [
-    'Content-Type: application/json',
-    'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
-]);;
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-
+curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
 $response = curl_exec($ch);
-
-if (curl_errno($ch)) {
-    echo 'Ошибка cURL: ' . curl_error($ch);
-} else {
-    $data = json_decode($response, true);
-    if (isset($data['result'])) {
-        echo '<pre>';
-        print_r($data['result']);
-        echo '</pre>';
-    } else {
-        echo 'Ошибка получения данных: ' . $response;
-    }
-}
-
 curl_close($ch);
+
+// Обрабатываем ответ
+$data = json_decode($response, true);
+if (isset($data['result'])) {
+    echo '<pre>';
+    print_r($data['result']);
+    echo '</pre>';
+} else {
+    echo 'Ошибка получения данных: ' . $response;
+}
 ?>
