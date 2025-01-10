9e8d20d415b30fed07256a73f9ed91025e3698d8
TIvers <wlad2000vita@gmail.com>
2025-01-10 13:01:10 +0700

    Добавлено постраничное получение продуктов в get_products.php с обработкой ошибок

--- api/get_products.php
+++ api/get_products.php
@@ -7,30 +7,39 @@ $params = [
     'select' => ['ID', 'IBLOCK_ID', 'NAME', 'PRICE', 'QUANTITY'], 
     'start' => 0 
 ];
-//
-// Выполняем запрос
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $webhookUrl);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
-$response = curl_exec($ch);
-curl_close($ch);
 
-// Обрабатываем ответ
-$data = json_decode($response, true);
-if (isset($data['result'])) {
-    echo '<pre>';
-    print_r($data['result']);
-    echo '</pre>';
-} elseif (isset($data['error'])) {
-    if ($data['error'] === 'authorization_error') {
-        echo 'Ошибка авторизации: ' . $data['error_description'];
+$allProducts = [];
+do {
+    // Выполняем запрос
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $webhookUrl);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    // Обрабатываем ответ
+    $data = json_decode($response, true);
+    if (isset($data['result'])) {
+        $allProducts = array_merge($allProducts, $data['result']);
+        $params['start'] += count($data['result']);
+    } elseif (isset($data['error'])) {
+        if ($data['error'] === 'authorization_error') {
+            echo 'Ошибка авторизации: ' . $data['error_description'];
+            break;
+        } else {
+            echo 'Ошибка получения данных: ' . $data['error_description'];
+            break;
+        }
     } else {
-        echo 'Ошибка получения данных: ' . $data['error_description'];
+        echo 'Ошибка получения данных: ' . $response;
+        break;
     }
-} else {
-    echo 'Ошибка получения данных: ' . $response;
-}
+} while (count($data['result']) > 0);
+
+echo '<pre>';
+print_r($allProducts);
+echo '</pre>';
 ?>
