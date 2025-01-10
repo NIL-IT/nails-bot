e4eae1480e49fe69dd74299b5060bc4504770438
TIvers <wlad2000vita@gmail.com>
2025-01-10 13:12:20 +0700

    Добавлено использование базовой аутентификации в запросе к вебхуку в get_products.php

--- api/get_products.php
+++ api/get_products.php
@@ -7,18 +7,20 @@ $params = [
     'select' => ['ID', 'IBLOCK_ID', 'NAME', 'PRICE', 'QUANTITY'], 
     'start' => 0 
 ];
-//
-// Выполняем запрос
+
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $webhookUrl);
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    'Content-Type: application/json',
+]);
+curl_setopt($ch, CURLOPT_USERPWD, 'wizzardhtt@gmail.com:111222'); // Логин и пароль
+
 $response = curl_exec($ch);
 curl_close($ch);
 
-// Обрабатываем ответ
 $data = json_decode($response, true);
 if (isset($data['result'])) {
     echo '<pre>';
