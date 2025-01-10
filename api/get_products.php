a0d194d9b17270b60ac18cff1ccf6f51308bcdd3
TIvers <wlad2000vita@gmail.com>
2025-01-10 13:57:19 +0700

    Оптимизирован код в get_products.php для упрощения обработки ответа и удаления лишнего цикла

--- api/get_products.php
+++ api/get_products.php
@@ -4,42 +4,31 @@ $webhookUrl = 'https://shtuchki.pro/rest/68/zhc69jnwgx6hweyj/catalog.product.lis
 
 $params = [
     'filter' => ['iblockId' => 21], 
-    'select' => ['id', 'iblockId', 'name', 'price', 'QUANTITY'], 
-    'start' => 0,
-    'limit' => 100 // Устанавливаем лимит на 100 товаров
+    'select' => ['id', 'iblockId', 'name', 'price', 'QUANTITY',"MINIMUM_PRICE","MAXIMUM_PRICE"], 
+    'start' => 0 
 ];
 
-$allProducts = [];
-do {
-    // Выполняем запрос
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $webhookUrl);
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, [
-        'Content-Type: application/json',
-    ]);
-    curl_setopt($ch, CURLOPT_USERPWD, 'wizzardhtt@gmail.com:111222'); // Логин и пароль
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $webhookUrl);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    'Content-Type: application/json',
+]);
+curl_setopt($ch, CURLOPT_USERPWD, 'wizzardhtt@gmail.com:111222'); // Логин и пароль
 
-    $response = curl_exec($ch);
-    curl_close($ch);
+$response = curl_exec($ch);
+curl_close($ch);
 
-    // Обрабатываем ответ
-    $data = json_decode($response, true);
-    if (isset($data['result'])) {
-        $allProducts = array_merge($allProducts, $data['result']);
-        $params['start'] += count($data['result']);
-    } elseif (isset($data['error'])) {
-        echo 'Ошибка получения данных: ' . $data['error_description'];
-        break;
-    } else {
-        echo 'Ошибка получения данных: ' . $response;
-        break;
-    }
-} while (count($data['result']) > 0);
-
-echo '<pre>';
-print_r($allProducts);
-echo '</pre>';
+$data = json_decode($response, true);
+if (isset($data['result'])) {
+    echo '<pre>';
+    print_r($data['result']);
+    echo '</pre>';
+} elseif (isset($data['error'])) {
+    echo 'Ошибка получения   данных: ' . $data['error_description'];
+} else {
+    echo 'Ошибка получения данных: ' . $response;
+}
 ?>
