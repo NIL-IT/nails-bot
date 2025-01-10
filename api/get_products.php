b9d052c689c5a92bee888f027e0c734489b2eb0e
TIvers <wlad2000vita@gmail.com>
2025-01-10 13:17:07 +0700

    Изменен URL вебхука и добавлена обработка ошибок cURL в get_products.php

--- api/get_products.php
+++ api/get_products.php
@@ -1,34 +1,32 @@
 <?php
+// Адрес REST API
+$webhookUrl = 'https://shtuchki.pro/rest/13283/hrwfpr2yee7qvk2f/profile/';
 
-$webhookUrl = 'https://shtuchki.pro/rest/13283/hrwfpr2yee7qvk2f/catalog.product.list';
-
-$params = [
-    'filter' => [],
-    'select' => ['ID', 'IBLOCK_ID', 'NAME', 'PRICE', 'QUANTITY'], 
-    'start' => 0 
-];
-
+// Инициализация cURL
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $webhookUrl);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
+curl_setopt($ch, CURLOPT_URL, $webhookUrl); // URL запроса
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, [
     'Content-Type: application/json',
 ]);
-curl_setopt($ch, CURLOPT_USERPWD, 'wizzardhtt@gmail.com:111222'); // Логин и пароль
 
+// Выполнение GET-запроса
 $response = curl_exec($ch);
-curl_close($ch);
 
-$data = json_decode($response, true);
-if (isset($data['result'])) {
-    echo '<pre>';
-    print_r($data['result']);
-    echo '</pre>';
-} elseif (isset($data['error'])) {
-    echo 'Ошибка получения данных: ' . $data['error_description'];
+// Проверка на ошибки
+if (curl_errno($ch)) {
+    echo 'Ошибка cURL: ' . curl_error($ch);
 } else {
-    echo 'Ошибка получения данных: ' . $response;
+    $data = json_decode($response, true);
+    if (isset($data['result'])) {
+        echo '<pre>';
+        print_r($data['result']);
+        echo '</pre>';
+    } else {
+        echo 'Ошибка получения данных: ' . $response;
+    }
 }
+
+// Закрытие cURL
+curl_close($ch);
 ?>
