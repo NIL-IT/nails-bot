70d597317dd92a7542853bf73128e0d60fc450b7
TIvers <wlad2000vita@gmail.com>
2025-01-20 14:49:19 +0700

    11

--- api/get_products.php
+++ api/get_products.php
@@ -9,47 +9,35 @@ $select = [
     'detailText',
     'detailPicture',
     'purchasingPrice',
-    'iblockSectionId' // Добавляем поле iblockSectionId
 ];
 
 $params = [
-    'filter' => ['iblockId' => 21, 'iblockSectionId' => 1], // Фильтрация по iblockSectionId
+    'filter' => ['iblockId' => 21], 
     'select' => $select, 
-    'start' => 0,
-    'limit' => 50 // Устанавливаем лимит на 50 товаров
+    'start' => 0 
 ];
 
-$allProducts = [];
-do {
-    // Выполняем запрос
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $webhookUrl);
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, [
-        'Content-Type: application/json',
-    ]);
-    curl_setopt($ch, CURLOPT_USERPWD, 'wizzardhtt@gmail.com:111222'); // Логин и пароль
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $webhookUrl);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    'Content-Type: application/json',
+]);
 
-    $response = curl_exec($ch);
-    curl_close($ch);
 
-    // Обрабатываем ответ
-    $data = json_decode($response, true);
-    if (isset($data['result'])) {
-        $allProducts = array_merge($allProducts, $data['result']);
-        $params['start'] += count($data['result']);
-    } elseif (isset($data['error'])) {
-        echo 'Ошибка получения данных: ' . $data['error_description'];
-        break;
-    } else {
-        echo 'Ошибка получения данных: ' . $response;
-        break;
-    }
-} while (count($data['result']) > 0);
+$response = curl_exec($ch);
+curl_close($ch);
 
-echo '<pre>';
-print_r($allProducts);
-echo '</pre>';
+$data = json_decode($response, true);
+if (isset($data['result'])) {
+    echo '<pre>';
+    print_r($data['result']);
+    echo '</pre>';
+} elseif (isset($data['error'])) {
+    echo 'Ошибка получения   данных: ' . $data['error_description'];
+} else {
+    echo 'Ошибка получения данных: ' . $response;
+}
 ?>
