d0bf9ed915849ba2c429e9b93431851c571d0db7
TIvers <wlad2000vita@gmail.com>
2025-01-23 17:28:25 +0700

    Упрощено получение товаров в get_products.php, удален постраничный процесс и добавлена обработка ошибок при получении данных.

--- api/get_products.php
+++ api/get_products.php
@@ -9,54 +9,36 @@ $select = [
     'detailText',
     'detailPicture',
     'purchasingPrice',
+    
 ];
 
-$step = 50;
-$startFile = '/var/www/testingnil6/backend/api/start.txt';
-$productFile = '/var/www/testingnil6/backend/api/products.txt';
+$params = [
+    'filter' => ['iblockId' => 21], 
+    'select' => $select, 
+    'start' => 30450 
+];
 
-// Чтение значения start из файла
-if (file_exists($startFile)) {
-    $start = (int)file_get_contents($startFile);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $webhookUrl);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    'Content-Type: application/json',
+]);
+
+
+$response = curl_exec($ch);
+curl_close($ch);
+
+$data = json_decode($response, true);
+if (isset($data['result'])) {
+    echo '<pre>';
+    print_r($data['result']);
+    echo '</pre>';
+} elseif (isset($data['error'])) {
+    echo 'Ошибка получения   данных: ' . $data['error_description'];
 } else {
-    $start = 0;
+    echo 'Ошибка получения данных: ' . $response;
 }
-
-$file = fopen($productFile, 'a');
-
-do {
-    $params = [
-        'filter' => ['iblockId' => 21], 
-        'select' => $select, 
-        'start' => $start 
-    ];
-
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $webhookUrl);
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, [
-        'Content-Type: application/json',
-    ]);
-
-    $response = curl_exec($ch);
-    curl_close($ch);
-
-    $data = json_decode($response, true);
-    if (isset($data['result'])) {
-        foreach ($data['result'] as $product) {
-            fwrite($file, print_r($product, true) . "\n");
-        }
-        $start += $step;
-        // Сохранение текущего значения start в файл
-        file_put_contents($startFile, $start);
-    } else {
-        break;
-    }
-} while (count($data['result']) > 0);
-
-fclose($file);
-
-echo 'Данные успешно сохранены в файл products.txt';
 ?>
