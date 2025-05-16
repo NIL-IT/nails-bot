05bdf9be3408192c59a54c81db9289f1f5085bcf
Danil Chuksin <chyksin@bk.ru>
2025-05-16 20:08:23 +0700

    fix links

--- src/components/routes/AppRoutes.jsx
+++ src/components/routes/AppRoutes.jsx
@@ -30,8 +30,8 @@ const AppRoutes = ({ user, categories }) => {
         <Route path={ROUTES.PRODUCT} element={<SingleProduct />} />
         <Route path={ROUTES.SEARCH} element={<SearchItems />} />
         <Route path={ROUTES.CHECKOUT} element={<Checkout user={user} />} />
-        {/* user?.admin */}
-        {true ? (
+
+        {user?.admin ? (
           <Route
             path={ROUTES.PROFILE}
             element={<OrderHistoryAdmin user={user} />}
