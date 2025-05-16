f6095340ed86c63efde0bf84da14a0df0b17ecd3
Danil Chuksin <chyksin@bk.ru>
2025-05-16 22:01:46 +0700

    fix links

--- src/components/ui/BackButton.jsx
+++ src/components/ui/BackButton.jsx
@@ -6,7 +6,7 @@ const BackButton = () => {
   const goBack = () => {
     if (window.Telegram?.WebApp?.isExpanded) {
       console.log("window.Telegram?.WebApp?.isExpanded");
-      Telegram.WebApp.close(); // Закрыть WebView полностью
+      navigate(-1); // Закрыть WebView полностью
     } else {
       console.log(window.location.href.includes("web"));
       navigate(-1); // Попробовать вернуться назад (если внутри мини-приложения)
