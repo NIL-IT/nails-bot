48592198613d3f4ef9623bd36c1315c80d169647
Кирилл Третьяков <k.tretyakov.98@gmail.com>
2025-06-27 11:03:46 +0700

    add checking quantity

--- api/check_item.php
+++ api/check_item.php
@@ -9,13 +9,14 @@ if (strpos($contentType, 'application/json') !== false) {
 } else {
     $data = $_POST;
 }
-header('Content-Type: application/json');
-
-$data = json_decode(file_get_contents('php://input'), true);
 $apiClient = new CatalogBitrixRestApiClient( 'https://shtuchki.pro/rest/13283/nj2nk4gedj6wvk5j/' );require_once 'DatabaseClient.php';
 
-$select = ['quantity','active','available'];
+$product = $apiClient -> getCatalogProduct($data['id_product']);
+if ($product['available'] === 'Y' and $product['active'] === 'Y' and $product['quantity'] != null) {
+    echo json_encode(['success' => true, 'data' => $data['id_product'],'active' => 'Y','quantity' => $product['quantity']]);
+}
+else {
+    echo json_encode(['success' => true, 'data' => $product,'active' => 'N']);
+}
 
-$result = $apiClient -> getCatalogProduct($data['id'], $select);
-print_r($result)
 ?>
